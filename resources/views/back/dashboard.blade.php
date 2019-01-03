da92edbc8a838b12297ba3ce3d512896766e29e4
Shadrach Odekhiran <shadymedy@gmail.com>
2019-01-03 15:44:45 +0100

    dashboard dato loading 1a

--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -5,30 +5,60 @@
     <div class="main-panel">
 
    @include('layouts.partial.topbar')
-        <div class="main-content" id="app">
+        <div class="main-content">
             <div class="container-fluid">
 
 
-                    <h4 class="title">Dashboards</h4>
+                <h4 class="title">Dashboards</h4>
                     <div class="row icon-resizer">
                         <div class="col-md-4">
                             <div class="card">
-                                <owner-component :totalDoc="{{ 300 }}"></owner-component>
+                                <div class="content">
+                                    <p class="text-muted">Total Document Owners</p>
+                                    <div class="row icon-resizer">
+                                        <div class="col-md-6 col-sm-6">
+                                            <h5 class="pull-left bounce"> {{ 600 }}</h5>
+                                        </div>
+                                        <div class="col-md-6 col-sm-6">
+                                            <i class="pe-7s-copy-file pull-right"></i>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div class="card">
-                                <new-reg-component></new-reg-component>
+                                <div class="content">
+                                    <p class="text-muted">New Document Registration</p>
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-6">
+                                            <h5 class="pull-left"> {{ 66 }}</h5>
+                                        </div>
+                                        <div class="col-md-6 col-sm-6">
+                                            <i class="pe-7s-copy-file pull-right"></i>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div class="card">
-                                <transfer-component></transfer-component>
+                                <div class="content">
+                                    <p class="text-muted">Document Ownership Transfer</p>
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-6">
+                                            <h5 class="pull-left"> {{ 5 }} </h5>
+                                        </div>
+                                        <div class="col-md-6 col-sm-6">
+                                            <i class="pe-7s-copy-file pull-right"></i>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                <div class="row">
+                <div class="row" id="app">
                     <graph-component></graph-component>
                 </div>
 
