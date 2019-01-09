9aadb67b5e47313d60097312987db69a701367e7
Shadrach Odekhiran <shadymedy@gmail.com>
2019-01-09 10:11:57 +0100

    adjust resource api 6

--- app/Http/Resources/Owner/OwnerResource.php
+++ app/Http/Resources/Owner/OwnerResource.php
@@ -31,7 +31,7 @@ class OwnerResource extends JsonResource
             'emailAddress' => $this->email_address,
             "passport" =>  $this->passport ? $this->passport->file : null,
             "signature" =>  $this->signature ? $this->signature->file : null,
-            'documents' => $this->documents
+          //  'documents' => $this->documents
 
 
         ];
